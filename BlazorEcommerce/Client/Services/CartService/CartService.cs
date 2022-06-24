a488c8165b2d74a481e85aed5ef12e6d6dc01c99
CS <CS@BOSSHOSS>
2022-06-24 12:14:08 -0500

    Added CartItem Quantity Update Functionality

--- BlazorEcommerce/Client/Services/CartService/CartService.cs
+++ BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -22,9 +22,21 @@ namespace BlazorEcommerce.Client.Services.CartService
             {
                 cart = new List<CartItem>();
             }
-            cart.Add(cartItem);
 
+            var sameItem = cart.Find(x => x.ProductId == cartItem.ProductId &&
+                x.ProductTypeId == cartItem.ProductTypeId);
+            
+            if (sameItem == null)
+            {
+                cart.Add(cartItem);
+            }
+            else
+            {
+                sameItem.Quantity += cartItem.Quantity;
+            }
+            
             await _localStorage.SetItemAsync("cart", cart);
+            
             OnChange.Invoke();
         }
 
@@ -62,6 +74,23 @@ namespace BlazorEcommerce.Client.Services.CartService
                 cart.Remove(cartItem);
                 await _localStorage.SetItemAsync("cart", cart);
                 OnChange.Invoke();
+            }
+        }
+
+        public async Task UpdateQuantity(CartProductResponse product)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            if (cart == null)
+            {
+                return;
+            }
+
+            var cartItem = cart.Find(x => x.ProductId == product.ProductId
+                && x.ProductTypeId == product.ProductTypeId);
+            if (cartItem != null)
+            {
+                cartItem.Quantity = product.Quantity;
+                await _localStorage.SetItemAsync("cart", cart);
             }
         }
     }

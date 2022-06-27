9aa858696975d6ad55d9dc7e47a24a78d0373988
CS <CS@BOSSHOSS>
2022-06-27 14:29:59 -0500

    Implemented Client Side Order Placement

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -2,8 +2,9 @@ global using BlazorEcommerce.Client.Services.AuthService;
 global using BlazorEcommerce.Client.Services.CartService;
 global using BlazorEcommerce.Client.Services.CategoryService;
 global using BlazorEcommerce.Client.Services.ProductService;
+global using BlazorEcommerce.Client.Services.OrderService;
 global using BlazorEcommerce.Shared;
-global using Microsoft.AspNetCore.Components.Authorization; 
+global using Microsoft.AspNetCore.Components.Authorization;
 global using System.Net.Http.Json;
 
 using BlazorEcommerce.Client;
@@ -12,10 +13,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+//Register Blazored Local Storage
 builder.Services.AddBlazoredLocalStorage();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 //Register Services for Dependency Injection
@@ -23,6 +27,7 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
 
 //Register Authentication State Provider
 builder.Services.AddOptions();
